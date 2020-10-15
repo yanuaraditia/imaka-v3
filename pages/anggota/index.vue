da0e70d7d2b4d62bd06ea2e8b9e0801e276133d1
Yanuar Aditia <yanuaraditia@outlook.com>
2020-10-15 17:15:11 +0700

    Add pencarian full front

--- pages/anggota/index.vue
+++ pages/anggota/index.vue
@@ -2,7 +2,8 @@
     <div>
         <section class="py-5">
             <div class="container-fluid my-lg-5 pt-lg-4">
-                <my-jumbotron title="Sedulur" desc="Sedulur"/>
+                <h1 class="ndes-1 f-2">Sedulur</h1>
+                <input type="text" class="form-control" v-if="isLoaded" @keyup="cariData" @change="cariData" v-model="pencarian" placeholder="Cari anggota">
                 <div class="row my-4 no-gutters peoples">
                     <div class="col-12 col-md-4 col-xl-3" v-for="(anggota, i) in displayedAnggota" :key="`${new Date().now}${i}`" @mouseover="prepAnggota(anggota)">
                         <nuxt-link :to="`/anggota/${anggota.card_number}`" class="card people">
@@ -12,6 +13,13 @@
                                 <small class="card-text">{{anggota.major.name}}</small>
                             </div>
                         </nuxt-link>
+                    </div>
+                    <div class="col-12 text-center py-5" v-if="notFound">
+                        <div class="text-muted mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-alert-triangle"><path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path><line x1="12" y1="9" x2="12" y2="13"></line><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
+                        </div>
+                        <h3 class="title">Data tidak ditemukan</h3>
+                        <p class="description"><span class="text-primary">{{pencarian}}</span> tidak ditemukan. <span @click="resetData">Reset ?</span></p>
                     </div>
                 </div>
                 <div class="d-md-flex align-items-md-center justify-content-md-center text-wrap">
@@ -37,18 +45,39 @@
 import axios from 'axios'
 export default {
     head: {
-        title: 'Anggota'
+        title: 'Anggota',
     },
     data() {
         return {
             isLoaded: false,
             anggotas: [],
+            pencarian: '',
             page: 1,
             perPage: 12,
             pages: [],
+            notFound: false
         }
     },
     methods: {
+        resetData() {
+            this.pencarian = ''
+            this.cariData
+            this.notFound = false
+            this.anggotas = JSON.parse(localStorage.anggotas)
+        },
+        cariData() {
+            var datas = JSON.parse(localStorage.anggotas)
+            this.anggotas = []
+            for(var i = 0; i < datas.length; i++) {
+                if(datas[i].name.toLowerCase().includes(this.pencarian.toLowerCase())) {
+                    this.anggotas.push(datas[i])                    
+                }
+            }
+
+            if(this.anggotas.length < 1) {
+                this.notFound = true
+            }
+        },
         prepAnggota(data) {
             localStorage[`a_${data.card_number}`] = JSON.stringify(data)
         },
