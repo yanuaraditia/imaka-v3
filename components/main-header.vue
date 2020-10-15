11aea7a9575e291d2f3f7675fa43483be3e47b90
Yanuar Aditia <yanuaraditia@outlook.com>
2020-10-15 15:29:15 +0700

    Center assault

--- components/main-header.vue
+++ components/main-header.vue
@@ -1,8 +1,9 @@
 <template>
   <b-navbar toggleable="lg" type="dark" v-bind:class="isFixed ? 'fixed' : 'rel'">
       <div class="container-fluid">
-        <b-navbar-brand to="/" class="mr-md-5 pr-md-3"><main-logo></main-logo></b-navbar-brand>
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-brand to="/" class="mr-md-5 pr-md-3"><main-logo></main-logo></b-navbar-brand>
+        <toggle-mobile></toggle-mobile>
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav class="px-0 px-lg-3 py-2 ml-auto mt-2 mt-md-0 p-md-0 justify-content-lg-end">
               <b-nav-item to="/anggota">Anggota</b-nav-item>
---
+++ components/toggle-mobile.vue
@@ -0,0 +1,42 @@
+<template>
+  <b-nav-item class="d-lg-none cuproy">
+    <button type="button" class="btn btn-link p-0 nav-link btn-block text-left text-white" role="button" aria-label="Toggle dark/light" @click.prevent="toggleTheme">
+        <svg v-if="darkTheme" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" width="27px" height="27px" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6"><path d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path></svg>
+        <svg v-else fill="none" stroke-linecap="round" stroke-linejoin="round" width="27px" height="27px" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6"><path d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
+    </button>
+  </b-nav-item>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      darkTheme: false,
+    }
+  },
+  methods: {
+    toggleTheme() {
+      this.darkTheme = !this.darkTheme
+      $nuxt.$colorMode.preference = this.darkTheme ? 'dark' : 'light'
+    }
+  },
+  mounted() {
+    if(localStorage['nuxt-color-mode'] == 'dark') {
+      this.darkTheme = true
+    }
+    $nuxt.$colorMode.preference = this.darkTheme ? 'dark' : 'light'
+  }
+}
+</script>
+<style lang="scss">
+.cuproy {
+    list-style: none;
+    .nav-link {
+        padding-left: .25rem;
+        padding-right: .25rem;
+        .btn-link {
+            color: #838383!important;
+        }
+    }
+}
+</style>
